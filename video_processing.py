9254b3d46767e01d56ca8d55fe7250e22451224c
Aashish Singh <aashishsingh@Aashishs-MacBook-Air.local>
2025-01-03 21:25:53 +0530

    Add merge api

--- video_processing.py
+++ video_processing.py
@@ -1,8 +1,10 @@
 import os 
 import sqlite3
+import subprocess
 from flask import Flask, request, jsonify
 
 SQL_DB = 'video_db'
+VIDEO_LOCAL_DIR = 'videos'
 app = Flask(__name__)
 
 
@@ -12,10 +14,9 @@ def upload_video():
     max_file_size = request.form.get('maximum_video_file_size')
     min_duration = request.form.get('minimum_video_duration')
     max_duration = request.form.get('maximum_video_duration')
-    upload_folder = 'videos'
-    os.makedirs(upload_folder)
+    os.makedirs(VIDEO_LOCAL_DIR)
     filename = file.filename
-    filepath = os.path.join(upload_folder, filename)
+    filepath = os.path.join(VIDEO_LOCAL_DIR, filename)
     file.save(filepath)
     with sqlite3.connect(SQL_DB) as conn:
         cursor = conn.cursor()
@@ -23,5 +24,61 @@ def upload_video():
         conn.commit()
         return jsonify({"message": "Video uploaded successfully", "filename": filename})
     return jsonify({"message": "Some errored occurred!", "filename": filename})
+
+@app.route('/trim', methods=['POST'])
+def trim_video():
+    trim_data = request.get_json()
+    filename = trim_data.get('filename')
+    start_time = trim_data.get('starttime')
+    end_time = trim_data.get('endtime')
+    filepath = os.path.join(VIDEO_LOCAL_DIR, filename)
+    trimmed_video_filepath = os.path.join(VIDEO_LOCAL_DIR, filename.replace('.','.trimmed'))
+    try:
+        subprocess.run([
+                "ffmpeg",
+                "-i", filepath,
+                "-ss", str(start_time), 
+                "-to", str(end_time),  
+                "-c", "copy", 
+                trimmed_video_filepath  
+            ],check=True)
+    except subprocess.CalledProcessError as e:
+        return jsonify({"error": "Error during video trimming", "details": str(e)})
+    return jsonify({"message": "Video trimmed successfully", "filename": trimmed_video_filepath})
+
+@app.route('/merge', methods=['POST'])
+def merge_videos():
+    merge_data = request.get_json()
+    filenames = merge_data.get('filenames', [])
+    merged_filename = 'merged_video.mp4'
+    merged_filepath = os.path.join(VIDEO_LOCAL_DIR, merged_filename)
+    filepaths = []
+    for filename in filenames:
+        filepath = os.path.join(VIDEO_LOCAL_DIR, filename)
+        filepaths.append(filepath)
+    file_list_path = os.path.join(VIDEO_LOCAL_DIR, "file_list.txt")
+    with open(file_list_path, 'w') as file_list:
+        for filepath in filepaths:
+            file_list.write(f"file '{filepath}'\n")
+    try:
+        subprocess.run(
+            [
+                "ffmpeg",
+                "-f", "concat",  
+                "-safe", "0",    
+                "-i", file_list_path,
+                "-c", "copy",    
+                merged_filepath
+            ],
+            check=True
+        )
+    except subprocess.CalledProcessError as e:
+        return jsonify({"error": "Error during video merging", "details": str(e)})
+    
+    return jsonify({"message": "Videos merged succesfully!", "filename": merged_filename})
+
+
+
+
 
 
